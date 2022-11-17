8feb3f58e9527396616cf5b9199746de5af4a47b
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-11-16 23:10:28 -0800

    Made html and added equations

---
+++ script.js
@@ -0,0 +1,14 @@
+const inputNumber = document.getElementById('input-number');
+const convertBtn = document.getElementById('convert-btn');
+const lengthConverter = document.getElementById('length-conversion');
+const volumeConverter = document.getElementById('volume-conversion');
+const massConverter = document.getElementById('mass-conversion');
+
+// meter to feet 	ft=m*3.2808
+// feet to meter m=ft/3.2808
+
+// liters to gallon 0.264172 × liters
+// gallon to liter liters = gallons × 3.785412
+
+// kg to pounds m(kg) / 0.45359237
+// pounds to kg lbs *  x 0.45359237 kg
